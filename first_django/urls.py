606ae540544958350eb25f529d51a6375f7d9627
PALAK1233000 <shik9195@gmail.com>
2024-10-20 16:34:38 +0530

    final changes

---
+++ first_django/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from first_django import views
+handler404 = 'first_django.views.handler404'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('polls/',include('polls.urls')),
+    path('api/',include('polls.api_urls')),
+    path('accounts/',include('accounts.urls')),
+]
